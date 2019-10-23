5636b857ab1e33912585367dc1550afb5e1c83e8
mikovar <mkovar@huskers.unl.edu>
2019-10-23 16:14:14 -0500

    Converted some double quotes to single quotes and converted to yarn from npm.

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
+    path: '/',
+    name: 'home',
     component: Home
   },
 ];
