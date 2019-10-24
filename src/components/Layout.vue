47dba8c2d6b805629c4c0924508e6f93b12b2496
mikovar <mkovar@huskers.unl.edu>
2019-10-24 18:09:41 -0500

    GH-5 Transitioned the layout to use a grid pattern and made css tags and containers for all of the ui parts.

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,10 +1,13 @@
 <template>
-  <div style="min-width: 64px; overflow: visible; position: relative;">
-    <navigation-bar />
-    <queue-drawer :drawer-open="open"></queue-drawer>
-    <v-content>
+  <div class="layoutGrid">
+    <navigation-bar class="gridNavigation" />
+    <queue-drawer :drawer-open="open" class="gridQueue" />
+    <v-content class="gridContent">
       <router-view></router-view>
     </v-content>
+    <div class="gridPlayer" style="border-style: solid;">
+      Player
+    </div>
   </div>
 </template>
 
@@ -24,3 +27,29 @@ export default {
   }
 };
 </script>
+<style scoped>
+  /* https://www.w3schools.com/css/css_grid.asp */
+  .layoutGrid {
+    height: 100%;
+    width: 100%;
+    display: grid;
+    grid-template-areas:
+      "navigation navigation navigation"
+      "queue content content "
+      "player player player";
+    grid-template-columns: max-content auto minmax(max-content, 64px);
+    grid-template-rows: max-content auto minmax(max-content, 64px);
+  }
+  .gridNavigation {
+    grid-area: navigation;
+  }
+  .gridQueue {
+    grid-area: queue;
+  }
+  .gridContent {
+    grid-area: content;
+  }
+  .gridPlayer {
+    grid-area: player
+  }
+</style>
--- src/components/QueueDrawer.vue
+++ src/components/QueueDrawer.vue
@@ -1,9 +1,9 @@
 <template>
   <!-- TODO Make this top style be bound to the height of navigation bar, or find another way not to overlap -->
-  <v-navigation-drawer permanent style="top: 64px; position: relative; height: 100%;">
-    <v-list style="background-color: #A4A4A4; overflow: scroll;" dense>
+  <v-navigation-drawer permanent style="top: 64px; position: relative; overflow-y: scroll">
+    <v-list style="background-color: #A4A4A4; height: 100%" dense>
       <h3 style="text-align: center;"> {{ queueTitle }} </h3>
-      <v-list-item style="background-color: #D1D1D1" v-for="song in songs" :key="song.id" color="purple" selectable>
+      <v-list-item style="background-color: #D1D1D1" v-for="song in songs" :key="song.title + song.platform" color="purple" selectable>
         {{ song.title }} - {{ song.artist }}
       </v-list-item>
     </v-list>
@@ -30,40 +30,14 @@ export default {
     songs: function () {
       return [
         {
-          id: 0,
           title: 'Red Eyes',
           artist: 'Chills',
-          album: 'unknown'
+          platform: 'Spotify'
         },
         {
-          id: 0,
           title: 'Red Eyes',
           artist: 'Chills',
-          album: 'unknown'
-        },
-        {
-          id: 0,
-          title: 'Red Eyes',
-          artist: 'Chills',
-          album: 'unknown'
-        },
-        {
-          id: 0,
-          title: 'Red Eyes',
-          artist: 'Chills',
-          album: 'unknown'
-        },
-        {
-          id: 0,
-          title: 'Red Eyes',
-          artist: 'Chills',
-          album: 'unknown'
-        },
-        {
-          id: 0,
-          title: 'Red Eyes',
-          artist: 'Chills',
-          album: 'unknown'
+          platform: 'Spotify'
         }
       ];
     }
