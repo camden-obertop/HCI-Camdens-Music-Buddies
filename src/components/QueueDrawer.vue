38c327111a39885f71b6b42cb8e37cb34f0be4a2
Thomas Braccia <bracciata@gmail.com>
2019-10-26 14:59:59 -0500

    Fixed navigation drawer overlap

--- src/components/QueueDrawer.vue
+++ src/components/QueueDrawer.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- TODO Make this top style be bound to the height of navigation bar, or find another way not to overlap -->
-  <v-navigation-drawer permanent style="top: 64px; position: relative; overflow-y: scroll">
+  <v-navigation-drawer permanent style="height:calc(100% - 64px);top: 64px; position: relative; overflow-y: scroll">
     <v-list style="background-color: #A4A4A4; height: 100%" dense>
       <h3 style="text-align: center;"> {{ queueTitle }} </h3>
       <v-list-item style="background-color: #D1D1D1" v-for="song in songs" :key="song.title + song.platform" color="purple" selectable>
